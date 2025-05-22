862fce653eb2370d814c3b6c75ee30d9261027fc
aliyarfnaura <2310817120014@mhs.ulm.ac.id>
2025-05-22 10:13:44 +0800

    Menambahkan project MODUL-4 ke repository Pemrograman-Mobile

---
+++ app/src/main/java/com/example/moviescrollablelist_viewmodel/data/movieList.kt
@@ -0,0 +1,104 @@
+package com.example.moviescrollablelist_viewmodel.data
+
+import com.example.moviescrollablelist_viewmodel.R
+
+val movieList = listOf(
+    MovieData(
+        id =  1,
+        name = "Ada Apa Dengan Cinta?",
+        image = R.drawable.aadc,
+        url = "https://letterboxd.com/film/whats-up-with-cinta/",
+        description = "Sinopsis: Ada Apa dengan Cinta? adalah film drama remaja Indonesia yang mengisahkan Cinta, siswi SMA populer yang hobi menulis puisi. Hidupnya berubah saat ia jatuh cinta pada Rangga, siswa pendiam dari latar belakang berbeda. Hubungan mereka yang tumbuh diam-diam membuat Cinta berkonflik dengan sahabat-sahabatnya, memaksanya memilih antara persahabatan atau cinta. Film ini menjadi ikon kisah cinta remaja Indonesia dengan sentuhan puisi dan emosi yang mendalam. Dibalut dengan alunan musik yang ikonik dan puisi-puisi yang menyentuh hati. Ada Apa dengan Cinta? menjadi simbol romantika masa muda yang membekas hingga kini."
+    ),
+    MovieData(
+        id =  2,
+        name = "Gie",
+        image = R.drawable.gie,
+        url = "https://letterboxd.com/film/gie/",
+        description = "Sinopsis: GIE adalah film biografi tentang Soe Hok Gie, aktivis dan penulis idealis yang lantang menyuarakan kebenaran di tengah gejolak politik era Soekarno dan Soeharto. Sebagai mahasiswa yang kritis, Gie menolak segala bentuk penindasan dan korupsi, meski harus berjuang seorang diri. Film ini menggambarkan perjuangan moral, pencarian makna hidup, dan semangat muda yang tak gentar melawan ketidakadilan. Dengan latar sejarah Indonesia yang penuh gejolak, GIE menjadi potret reflektif tentang suara kritis generasi muda, pentingnya integritas moral, dan keberanian untuk berdiri sendiri dalam memperjuangkan kebenaran, bahkan ketika itu berarti melawan arus."
+    ),
+    MovieData(
+        id =  3,
+        name = "Janji Joni",
+        image = R.drawable.janji_joni,
+        url = "https://letterboxd.com/film/jonis-promise/",
+        description = "Sinopsis: Janji Joni mengisahkan Joni, seorang pengantar gulungan film yang berjuang menepati janji kepada gadis impiannya untuk mengantar film tepat waktu. Namun, di tengah kota yang penuh kekacauan dan kejadian tak terduga, misi sederhana itu berubah menjadi petualangan kocak dan penuh tantangan. Dengan gaya penceritaan yang segar, musik yang catchy, dan nuansa urban yang kuat, Janji Joni menyuguhkan kisah tentang ketekunan, integritas, dan usaha seorang pria biasa yang ingin menepati janji demi cinta yang tumbuh dalam sekejap."
+    ),
+    MovieData(
+        id =  4,
+        name = "Catatan Akhir Sekolah",
+        image = R.drawable.cas,
+        url = "https://letterboxd.com/film/azzamine/",
+        description = "Sinopsis: Kisah tiga sahabat di SMA Fajar Harapan: Agni, Alde, dan Arian. Di sekolah mereka dianggap pecundang dan tak diperhitungkan di sekolah. Karena sifat itu pula Agni kehilangan pacarnya, Alina yang lebih memilih Ray, jagoan sekolah. Tiga orang itu bersahabat karena menjadi bulan-bulanan panitia orientasi pelajar saat jadi murid baru. Tiga sahabat tadi kemudian berambisi membuktikan kemampuan mereka. Agni yang berambisi menjadi sutradara film, lalu berniat membuat film dokumenter tentang sekolahnya yang ingin diputar pada pesta akhir tahun sekolah. Dibantu sahabatnya yang pandai menulis dan pandai main gitar bas, mereka membuat film dokumenter tentang dinamika kehidupan sekolah: ada cewek yang menyadari dia berada di bawah kekuasaan lelaki, ada kepala sekolah yang ditelanjangi kebejatannya."
+    ),
+    MovieData(
+        id =  5,
+        name = "Marmut Merah Jambu",
+        image = R.drawable.marmut_merah_jambu,
+        url = "https://letterboxd.com/film/pink-guinea-pig/details/",
+        description = "Sinopsis: Marmut Merah Jambu menceritakan Dika, siswa SMA canggung yang diam-diam menyukai gadis bernama Ina. Untuk menarik perhatiannya, ia membentuk Trio Detektif bersama dua sahabatnya dan mulai menyelidiki kasus misterius \"Demonic Pink Guinea Pig\" yang menghebohkan sekolah. Di tengah penyelidikan yang penuh kekonyolan dan kejadian absurd, Dika justru menemukan makna persahabatan, penerimaan diri, dan cinta remaja yang tak selalu berjalan sesuai harapan. Film ini menyuguhkan kisah cinta pertama dengan humor khas Raditya Dika yang segar dan menghibur. "
+    ),
+    MovieData(
+        id =  6,
+        name = "Aruna dan Lidahnya",
+        image = R.drawable.aruna,
+        url = "https://letterboxd.com/film/aruna-her-palate/",
+        description = "Sinopsis: Aruna dan Lidahnya mengisahkan Aruna, seorang epidemiolog yang gemar makan, saat ia melakukan perjalanan ke berbagai daerah untuk menyelidiki kasus flu burung. Bersama dua sahabatnya—seorang koki dan kritikus makanan—perjalanan dinas itu berubah menjadi petualangan kuliner yang penuh rasa, tawa, dan konflik. Di tengah sajian makanan khas Nusantara, Aruna mulai menyadari makna cinta, persahabatan, dan pencarian jati diri. Lewat visual yang menggoda selera dan dialog yang cerdas, Aruna dan Lidahnya menyuguhkan lebih dari sekadar kisah cinta dan makanan—ini adalah perjalanan emosional yang mempertemukan rasa, memori, dan hati. "
+    ),
+    MovieData(
+        id =  7,
+        name = "Ali dan Ratu Ratu Queens",
+        image = R.drawable.queens,
+        url = "https://letterboxd.com/film/ali-ratu-ratu-queens/",
+        description = "Sinopsis: Ali dan Ratu-Ratu Queens mengikuti perjalanan seorang remaja bernama Ali yang, setelah kehilangan ayahnya, pergi ke New York untuk mencari ibu kandungnya yang telah lama terpisah darinya. Di kota besar itu, Ali bertemu dengan sekelompok wanita Indonesia yang memberinya dukungan dan rasa kebersamaan. Di tengah pencariannya, Ali menemukan cinta pertama dan membangun hubungan yang mengubah hidupnya. Sebuah kisah tentang keluarga, cinta, dan pencarian jati diri yang penuh kehangatan. "
+    ),
+    MovieData(
+        id =  8,
+        name = "Mencuri Raden Saleh",
+        image = R.drawable.raden_saleh,
+        url = "https://letterboxd.com/film/stealing-raden-saleh/",
+        description = "Sinopsis: Mencuri Raden Saleh mengisahkan seorang ahli pembuat lukisan palsu yang merencanakan pencurian terbesar untuk menyelamatkan ayahnya. Ia membentuk tim yang terdiri dari para ahli untuk mencuri lukisan Raden Saleh yang sangat berharga. Dalam perjalanan yang penuh ketegangan dan rintangan, mereka menghadapi tantangan dari pihak berwenang dan musuh-musuh yang siap menggagalkan misi mereka. Sebuah film aksi thriller yang menggali tema persahabatan, pengorbanan, dan moralitas, dengan alur cerdas dan penuh kejutan."
+    ),
+    MovieData(
+        id =  9,
+        name = "The Big 4",
+        image = R.drawable.big_4,
+        url = "https://letterboxd.com/film/the-big-4/",
+        description = "Sinopsis: The Big 4 mengisahkan Dina, seorang detektif perempuan yang teguh mengikuti aturan, namun harus bergabung dengan empat pembunuh bayaran yang eksentrik dan sedang terpuruk dalam hidup mereka. Ketika ayah Nina dibunuh dengan cara misterius, ia terpaksa bekerja sama dengan tim yang kacau ini untuk mengungkap kebenaran. Dalam penyelidikan yang penuh kekacauan, aksi, dan komedi, mereka mulai menggali konspirasi besar yang melibatkan dunia kriminal yang berbahaya. Meskipun sering bertentangan, Dina dan timnya belajar untuk saling melengkapi dan bekerja sama, membentuk ikatan yang tak terduga dalam upaya mereka mencari keadilan."
+    ),
+    MovieData(
+        id =  10,
+        name = "A+",
+        image = R.drawable.aplus,
+        url = "https://letterboxd.com/film/a-2023/",
+        description = "Sinopsis: Berjuang dari barisan try out untuk menembus peringkat pertama, Kaliypso Dirgantari harus menghadapi empat besar pemegang tahta di SMA Bina Indonesia: Re Dirgantara, Kenan Aditya, Adinda Aletheia dan Aurora Calista. Masing-masing didorong oleh motivasi mereka sendiri, kelimanya bersaing ketat untuk mendapatkan peringkat paralel, setidaknya sampai rahasia epik terkait sistem sekolah terungkap."
+    ),
+    MovieData(
+        id =  11,
+        name = "Gadis Kretek",
+        image = R.drawable.gadis_kretek,
+        url = "https://letterboxd.com/film/cigarette-girl-2023/",
+        description = "Sinopsis: Soeraja merupakan pemilik pabrik kretek Djagad Raya yang sedang sekarat. Namun, ia justru ingin bertemu perempuan yang bukan istrinya, yakni Jeng Yah. Sang istri pun cemburu karena hal tersebut merupakan permintaan terakhir suaminya. Akan tetapi, ketiga anak Soeraja, yakni Lebas, Karim, dan Tegar tetap berusaha mencari keberadaan Jeng Yah ke pelosok Pulau Jawa. Ketika berada dalam perjalanan untuk mencari jejak Jeng Yah, mereka bertemu buruh batil yang menguak asal-usul Kretek Djagad Raya hingga menjadi kretek nomor satu di Indonesia dan juga mengetahui kisah cinta sang ayah dengan Jeng Yah."
+    ),
+    MovieData(
+        id =  12,
+        name = "Home Sweet Loan",
+        image = R.drawable.home_sweet_loan,
+        url = "https://letterboxd.com/film/home-sweet-loan/",
+        description = "Sinopsis: Home Sweet Loan mengisahkan perjuangan Kaluna, seorang wanita muda yang bekerja di bank dengan gaji pas-pasan. Kaluna bermimpi memiliki rumah sendiri di Jakarta, namun menghadapi berbagai tantangan finansial.  Ia tinggal bersama keluarga besarnya yang terdiri dari orang tua, dua kakak beserta keluarga mereka dalam satu rumah yang sempit. Bersama tiga sahabatnya Tanisha, Kamamiya, dan Danan - Kaluna berusaha keras mencari hunian terjangkau di pinggiran Jakarta. Ia harus memilih antara memperjuangkan mimpinya sendiri atau membantu keluarganya yang terlilit masalah utang. Film ini menggambarkan realita generasi sandwich dalam menghadapi tekanan finansial dan keluarga. Mereka rela mengerem pengeluaran dan mencari kerja sampingan demi mewujudkan impian tersebut. Namun, Kaluna dihadapkan pada dilema saat kondisi keuangan keluarganya memburuk dan membutuhkan bantuan.  "
+    ),
+    MovieData(
+        id =  13,
+        name = "Dua Hati Biru",
+        image = R.drawable.dua_hati_biru,
+        url = "https://letterboxd.com/film/home-sweet-loan/",
+        description = "Sinopsis: Kisah Bima dan Dara berlanjut. Pasangan suami-istri muda yang dulu menikah karena sebuah kesalahan. Empat tahun berlalu, Dara memutuskan kembali ke Jakarta demi berkumpul dengan keluarga kecilnya, Bima dan Adam. Berpisah sejak Adam lahir, Dara harus berusaha mendekatkan diri dengan sang buah hati meskipun terdapat berbagai masalah dalam rumah tangganya. "
+    ),
+    MovieData(
+        id =  14,
+        name = "Jumbo",
+        image = R.drawable.jumbo,
+        url = "https://letterboxd.com/film/jumbo-2025/",
+        description = "Sinopsis: Jumbo bercerita tentang Don, seorang anak yatim piatu berusia 10 tahun dengan tubuh besar yang sering diremehkan. Don menemukan pelarian dan inspirasi dalam buku dongeng warisan orang tuanya, yang penuh cerita ajaib. Untuk membuktikan dirinya, Don bertekad mengikuti pertunjukan bakat dengan sandiwara yang terinspirasi dari buku tersebut. Namun, mimpi Don dihina oleh teman-temannya, dan buku dongengnya dicuri oleh perundung. Untungnya, Don mendapat dukungan dari Oma dan sahabat-sahabatnya. Dalam usahanya mendapatkan kembali bukunya, Don bertemu dengan Meri, seorang anak dari dunia lain yang butuh bantuan untuk menemukan orang tuanya. Petualangan penuh keajaiban pun dimulai, mengajarkan arti persahabatan, keberanian, dan kepercayaan diri. "
+    ),
+)
